34c1c84e6d0fca554097a3040b424399ad173a40
George Tian <georgetian3@gmail.com>
2021-10-08 23:52:02 +1000

    Update typoer.py

--- typoer.py
+++ typoer.py
@@ -2,14 +2,19 @@ import keyboard
 import random
 from time import sleep
 
-def typoer(text, wpm = 100, accuracy = 1, wait_key = '', break_key = ''):
+# text: the text to be typed
+# wpm: average typing speed, measured in words per minute
+# accuracy: float between 0 and 1, higher accuracy means less typos
+# backspace_duration: time taken for backspace to be pressed
+# correction_coefficient: determines how many typos are made before correcting them,
+#                         lower coefficient means typos are caught quicker
+# wait_key: press this key to start typing
+# break_key: press this key to stop typing
+
+
+def typoer(text, wpm = 100, accuracy = 1, backspace_duration = 0.1, correction_coefficient = 0.4, wait_key = '', break_key = 'escape'):
 
     chars = list('abcdefghijklmnopqrstuvwxyz')
-
-    if accuracy < 0:
-        accuracy = 0
-    elif accuracy > 1:
-        accuracy = 1
 
     # seconds per character, using the average of 5 characters per word
     spc = 12 / wpm
@@ -31,11 +36,11 @@ def typoer(text, wpm = 100, accuracy = 1, wait_key = '', break_key = ''):
             return
 
         # correct typos
-        if typos and (i + typos >= len(text) or random.random() < 1 - 0.5 ** typos):
-            sleep(0.2)
+        if typos and (i + typos >= len(text) or random.random() < 1 - correction_coefficient ** typos):
+            sleep(backspace_duration)
             for _ in range(typos):
                 keyboard.press_and_release('backspace')
-                sleep(0.2)
+                sleep(backspace_duration)
             typos = 0
         # make typo
         if random.random() > accuracy: 
@@ -54,10 +59,9 @@ def typoer(text, wpm = 100, accuracy = 1, wait_key = '', break_key = ''):
         sleep(duration)
 
 
-text = r'''Lorem Ipsum is simply dummy text of the printing and typesetting industry.'''
-
 
 if __name__ == '__main__':
-    typoer(text, wpm = 100, accuracy = 0.95, wait_key = 'up', break_key = 'down')
+    text = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+    typoer(text, wpm = 120, accuracy = 0.8, wait_key = 'right')
         
     
